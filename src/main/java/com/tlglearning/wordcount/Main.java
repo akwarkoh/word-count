bf7c845e9da7662c8a1b3dd716dbf5e3e63c7531
akwarkoh <angelakwarko@gmail.com>
2022-07-22 17:22:11 -0700

    Initial commit of word counting application

---
+++ src/main/java/com/tlglearning/wordcount/Main.java
@@ -0,0 +1,39 @@
+package com.tlglearning.wordcount;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+  private static final String TEST_FILE_NAME= "hound-of-the-baskervilles.txt";
+  public static void main(String[] args) throws IOException {
+    try (
+        InputStream input=Main.class.getClassLoader().getResourceAsStream(TEST_FILE_NAME);
+        Reader reader=new InputStreamReader(input);
+        BufferedReader buffer=new BufferedReader(reader)
+        ) {
+
+          WordCounter counter = new WordCounter();
+          String line;
+          while((line =buffer.readLine())!=null) {
+
+            counter.add(line);
+          }
+
+            System.out.println(counter);
+
+
+          }
+        }
+
+
+}
---
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -0,0 +1,64 @@
+package com.tlglearning.wordcount;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public final class WordCounter {
+
+  private final Map<String, Integer> counts = new HashMap<>();
+
+  private int totalWords;
+
+
+  public Set<String> words() {
+    return counts.keySet();
+  }
+
+  public int get(String word) {
+    return counts.getOrDefault(word, 0);
+  }
+
+  public Map<String, Integer> getCounts() {
+    return Collections.unmodifiableMap(counts);
+  }
+
+  public void add(String text) {
+    String[] words = splitWords(text);
+    countWords(words);
+  }
+
+
+  @Override
+  public String toString() {
+    return counts.toString();
+
+  }
+
+
+  void countWords(String[] words) {
+    for (String word : words)
+      counts.put(word, get(word) + 1);
+      totalWords++;
+
+  }
+
+
+  public int total() {
+    return totalWords;
+
+
+  }
+    public int size() {
+      return counts.size();
+  }
+
+    String[] splitWords (String text){
+      return text
+          .trim()
+          .toLowerCase()
+          .split("[\\W_]+");
+    }
+
+  }

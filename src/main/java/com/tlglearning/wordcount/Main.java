516050095e65af98ec8c8ad0db6b3792b20cc56a
akwarkoh <angelakwarko@gmail.com>
2022-08-03 15:46:44 -0700

    javadoc in progress

--- src/main/java/com/tlglearning/wordcount/Main.java
+++ src/main/java/com/tlglearning/wordcount/Main.java
@@ -11,29 +11,54 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.Map.Entry;
 
+
+/**
+ * Demonstrates the practice of word counting, used (for example) in authorship studies. This
+ * demonstration uses (in the included methods) implementations of the {@link java.util.Map},
+ * {@link java.util.Map.Entry}, and {@link java.util.stream.Stream} interfaces.
+ */
 public class Main {
 
-  private static final String TEST_FILE_NAME= "hound-of-the-baskervilles.txt";
+  private static final String TEST_FILE_NAME = "hound-of-the-baskervilles.txt";
+  /**
+   * Creates an instance of {@link WordCounter}, using it to process the contents of "The Hound of
+   * the Baskervilles", by Arthur Conan Doyle. After processing, the word counts (excluding short
+   * words) are listed in descending order.
+   * <p>This method assumes that a text file named {@code hound-of-the-baskervilles.txt} is located
+   * on the classpath; otherwise (or if that file cannot be read for some reason), an instance of
+   * {@link IOException} is thrown.</p>
+   *
+   * @param args Command-line arguments (currently ignored).
+   * @throws IOException Thrown if input file cannot be located or read.
+   */
+
   public static void main(String[] args) throws IOException {
     try (
-        InputStream input=Main.class.getClassLoader().getResourceAsStream(TEST_FILE_NAME);
-        Reader reader=new InputStreamReader(input);
-        BufferedReader buffer=new BufferedReader(reader)
-        ) {
+        InputStream input = Main.class.getClassLoader().getResourceAsStream(TEST_FILE_NAME);
+        Reader reader = new InputStreamReader(input);
+        BufferedReader buffer = new BufferedReader(reader)
+    ) {
 
-          WordCounter counter = new WordCounter();
-          String line;
-          while((line =buffer.readLine())!=null) {
+      WordCounter counter = new WordCounter();
+      String line;
+      while ((line = buffer.readLine()) != null) {
 
-            counter.add(line);
-          }
+        counter.add(line);
+      }
 
-            System.out.println(counter);
+      counter
+          .getCounts()
+          .entrySet()
+          .stream()
+          .sorted(Comparator.comparing(Entry<String, Integer>::getValue).reversed())
+          .limit(10)
+          .forEach(System.out::println);
 
-
-          }
-        }
+    }
+  }
 
 
 }
--- src/main/java/com/tlglearning/wordcount/WordCounter.java
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -1,16 +1,19 @@
 package com.tlglearning.wordcount;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 
-public final class WordCounter {
+public class WordCounter {
+
+  private static final Set<String> BORING_WORDS = Set.of("and", "of", "the", "in", "on", "i", "then", "than", "out", "a", "if");
 
   private final Map<String, Integer> counts = new HashMap<>();
 
   private int totalWords;
-
 
   public Set<String> words() {
     return counts.keySet();
@@ -25,40 +28,40 @@ public final class WordCounter {
   }
 
   public void add(String text) {
-    String[] words = splitWords(text);
-    countWords(words);
+    String trimmedLine = text.trim();
+    if (!trimmedLine.isEmpty()) {
+      String[] words = splitWords(trimmedLine);
+      countWords(words);
+    }
   }
 
+  public int size() {
+    return counts.size();
+  }
+
+  public int total() {
+    return totalWords;
+  }
 
   @Override
   public String toString() {
     return counts.toString();
-
   }
 
+  String[] splitWords(String text) {
+    return text
+        .toLowerCase()
+        .split("[\\W_]+");
+  }
 
   void countWords(String[] words) {
-    for (String word : words)
-      counts.put(word, get(word) + 1);
-      totalWords++;
-
+    Arrays
+        .stream(words)
+        .map(String::trim)
+        .filter((word) -> !word.isEmpty())
+        .filter((word) -> word.length() > 5)
+//        .filter(Predicate.not(String::isEmpty))
+        .forEach((word) -> counts.put(word, 1 + counts.getOrDefault(word, 0)));
   }
 
-
-  public int total() {
-    return totalWords;
-
-
-  }
-    public int size() {
-      return counts.size();
-  }
-
-    String[] splitWords (String text){
-      return text
-          .trim()
-          .toLowerCase()
-          .split("[\\W_]+");
-    }
-
-  }
+}
